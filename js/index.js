fe2afd5fdb199ea58c44f7095e65eccbc510fc2e
Colin Thompson <thompsoncolinm@gmail.com>
2018-04-18 13:01:44 -0400

    added babel

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-// const { createElement } = React // removed for JSX
+const { createElement } = React // removed for JSX
 const { render } = ReactDOM
 
 /**
  * Removed for JSX
+ */
  
 const style = {
     backgroundColor: 'lightblue',
@@ -14,19 +15,19 @@ const style = {
      { id: 'title', className: 'header', style: style },
      'Hello World'
     )
-*/
     
 render(
-    // title, // removed for JSX
-    <h1 id='title' 
-    className='header' 
-    style={{
-        backgroundColor: 'lightblue',
-        color: 'white',
-        fontFamily: 'verdana'
-    }}>
-    Hello World
-    </h1>,
+    title, // removed for JSX
+    
+    // <h1 id='title' 
+    // className='header' 
+    // style={{
+    //     backgroundColor: 'lightblue',
+    //     color: 'white',
+    //     fontFamily: 'verdana'
+    // }}>
+    // Hello World
+    // </h1>,
     document.getElementById('app')
 );
 
---
+++ public/bundle.js
@@ -0,0 +1,26 @@
+const { createElement } = React; // removed for JSX
+const { render } = ReactDOM;
+
+/**
+ * Removed for JSX
+ */
+
+const style = {
+    backgroundColor: 'lightblue',
+    color: 'white',
+    fontFamily: 'verdana'
+};
+const title = createElement('h1', { id: 'title', className: 'header', style: style }, 'Hello World');
+
+render(title, // removed for JSX
+
+// <h1 id='title' 
+// className='header' 
+// style={{
+//     backgroundColor: 'lightblue',
+//     color: 'white',
+//     fontFamily: 'verdana'
+// }}>
+// Hello World
+// </h1>,
+document.getElementById('app'));
