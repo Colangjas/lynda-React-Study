75e090ac1908e30ab946b331f7f62583b5d02a5e
Colin Thompson <thompsoncolinm@gmail.com>
2018-04-18 16:16:54 -0400

    4:16 commit

--- public/bundle.js
+++ public/bundle.js
@@ -1,26 +1,52 @@
-const { createElement } = React; // removed for JSX
-const { render } = ReactDOM;
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _lib = require('./lib');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//const { createElement } = React // removed for JSX
+var render = _reactDom2.default.render;
 
 /**
  * Removed for JSX
- */
+ 
+ const style = {
+     backgroundColor: 'lightblue',
+     color: 'white',
+     fontFamily: 'verdana'
+    }
+    const title = createElement(
+        'h1',
+        { id: 'title', className: 'header', style: style },
+        'Hello World'
+    )
+    */
 
-const style = {
-    backgroundColor: 'lightblue',
-    color: 'white',
-    fontFamily: 'verdana'
-};
-const title = createElement('h1', { id: 'title', className: 'header', style: style }, 'Hello World');
+render(
+// title, // removed for JSX
+/*
+    <h1 id='title' 
+    className='header' 
+    style={{
+        backgroundColor: 'lightblue',
+        color: 'white',
+        fontFamily: 'verdana'
+    }}>
+    Hello World
+    </h1>,
+  */
 
-render(title, // removed for JSX
-
-// <h1 id='title' 
-// className='header' 
-// style={{
-//     backgroundColor: 'lightblue',
-//     color: 'white',
-//     fontFamily: 'verdana'
-// }}>
-// Hello World
-// </h1>,
-document.getElementById('app'));
+_react2.default.createElement(
+    'div',
+    null,
+    _lib.hello,
+    _lib.goodbye
+), document.getElementById('app'));
